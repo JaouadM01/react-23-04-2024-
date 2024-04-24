860379ff4326ea3d25b94983ecae6d0f1fc8a229
JaouadM01 <22156569@student.hhs.nl>
2024-04-24 09:11:23 +0200

    .

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import Dashboard from './routes/Dashboard.jsx';
+import Other from './routes/Other.jsx';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/dashboard', element: <Dashboard />},
+  {path: '/other', element: <Other />}
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ my-react-app/src/routes/Other.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Other = () => {
+  return (
+    <div>
+      <h3>Yup other I guess....</h3>
+    </div>
+  );
+};
+
+export default Other;
