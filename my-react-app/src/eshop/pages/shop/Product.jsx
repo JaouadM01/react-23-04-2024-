059e46de345c7d8d26c461659b4ef1642bb740f9
JaouadM01 <22156569@student.hhs.nl>
2024-04-30 12:18:32 +0200

    wrapping moet gefixed worden

--- my-react-app/src/eshop/pages/shop/Product.jsx
+++ my-react-app/src/eshop/pages/shop/Product.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 const Product = (props) => {
     const {id, productName, price, productImage } = props.data;
+    const {addToCart} = useContext(ShopContext)
     return <div className="product">
         <img src={productImage}></img>
         <div className="description">
             <b><p>{productName}</p></b>
             <p>${price}</p>
         </div>
-        <button className="addToCartBttn">Add To Cart</button>
+        <button className="addToCartBttn" onClick={() => {addToCart(id)}}>Add To Cart</button>
     </div>;
 }
 
